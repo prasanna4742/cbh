dff6a539506034d46768b5f94eae3df4d7c62541
prasanna4742 <kulkarni.prasanna@gmail.com>
2022-11-07 21:54:58 +0530

    Comment out pushing image to my repo

--- cbhapp/springapp/build.sh
+++ cbhapp/springapp/build.sh
@@ -2,4 +2,4 @@
 #Custom keycloak image with preconfigured realm for cbh app
 mvn clean install -DskipTests=true
 docker build -t prasannakulkarni/cbh-springapp:1.0.0 .
-docker push docker.io/prasannakulkarni/cbh-springapp:1.0.0
+#docker push docker.io/prasannakulkarni/cbh-springapp:1.0.0
--- cbhapp/springapp/buildTestContainer.sh
+++ cbhapp/springapp/buildTestContainer.sh
@@ -1,2 +1,2 @@
 docker build -f ./testContainer/TestContainerDockerFile -t prasannakulkarni/cbh-testapp:1.0.0 .
-docker push docker.io/prasannakulkarni/cbh-testapp:1.0.0
+#docker push docker.io/prasannakulkarni/cbh-testapp:1.0.0
