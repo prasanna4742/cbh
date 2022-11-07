b2051d9cd4d1b87a398dc872138c23ec83a97a92
prasanna4742 <kulkarni.prasanna@gmail.com>
2022-11-07 21:55:39 +0530

    Comment out pushing image to my repo

--- cbhapp/keycloak-cbhrealm/build.sh
+++ cbhapp/keycloak-cbhrealm/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #Custom keycloak image with preconfigured realm for cbh app
 docker build -t prasannakulkarni/cbh-keycloak:1.0.0 .
-docker push docker.io/prasannakulkarni/cbh-keycloak:1.0.0
+#docker push docker.io/prasannakulkarni/cbh-keycloak:1.0.0
